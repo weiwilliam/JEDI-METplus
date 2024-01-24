f8f37f600965f9dce813ad7f2ab7caaafd3ef155
weiwilliam <weiwilliam1987@gmail.com>
2024-01-24 16:08:30 -0600

    initial shape of JEDI-METplus workflow based on genint

---
+++ pyscripts/genint_vrfy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+import os
+import pandas as pd
+#import  
+
+
+start_date =
+end_date =
+srcpath = '/work2/noaa/jcsda/shihwei/git/genint_vrfy' #will replaced by path or file attributes.
+
+# paths for model and observations
+model_path =
+obs_path =
+
+# execute the genint_hofx3d
+input_yaml =
+
+
+# Check the outputs
+hofx_file
+
+# Update the StatAnalysis.conf
+# Issue the METplus
+
+# Collect the stats and plots
+
+
